746377de86dfcdf7c29e5cdad6e93dd6c8b26c94
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2024-05-05 22:24:06 -0300

    chore: add tailwindcss suport and change to eslint

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,61 +1,87 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    
-    opts = {
-      auto_install = true,
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup({
+                ensure_installed = {
+                    "tsserver",
+                    "tailwindcss-language-server",
+                    "eslint_d",
+                    "eslint"
+
+                }
+            })
+        end,
     },
-    config=function()
-      require("mason-lspconfig").setup({
-        ensure_installed={ "tsserver"}
-      })
-    end
-  },
-   
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local attach= function(_,_)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-        vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
-        vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", {})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
-      end
-      local lspconfig = require("lspconfig")
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
 
-      
-      lspconfig.tsserver.setup({
-        capabilities = capabilities,
-        on_attach=attach
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        on_attach=attach
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach=attach
-      })
+        opts = {
+            auto_install = true,
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "tsserver",
+                }
+            })
+        end
+    },
 
-     
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local attach = function(_, _)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+                vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+                vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {})
+                vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", {})
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+                vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
+            end
+            local lspconfig = require("lspconfig")
 
 
-      vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-      vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-      vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-      vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
-    end,
-  },
+
+
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+                on_attach = attach
+            })
+            lspconfig.html.setup({
+                capabilities = capabilities,
+                on_attach = attach
+            })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = attach
+            })
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                on_attach = attach,
+                falseroot_dir = function(...)
+                    return require("lspconfig.util").root_pattern(".git")(...)
+                end
+            })
+
+            lspconfig.cssls.setup({
+                capabilities = capabilities,
+                on_attach = attach
+            })
+
+
+
+
+
+            vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
+            vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
+            vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
+            vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+        end,
+    },
 }
---
+++ lua/plugins/tailwindcss.lua
@@ -0,0 +1,8 @@
+return {
+    "luckasRanarison/tailwind-tools.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    -- opts = {} -- your configuration
+    config = function()
+        require("tailwind-tools").setup({})
+    end
+}
